1d55537ef729e22c2fc8e2e18f5041049ee59cc7
S22F5 <s22f5@mailfence.com>
2023-03-17 13:33:39 +0100

    added kernel parameters

--- v2installer.sh
+++ v2installer.sh
@@ -81,8 +81,9 @@ outmsg=(
 "[37] Set xorg keymap to $KEYMAP"		#37
 "[38] Fixed some Permissions"			#38
 "[39] Blocking Webcam and Microphone"		#39	
-"[40] Unmounted Partitions"			#40
-"[41] !DONE! thanks for using this scripty"	#41
+"[40] Added Kernel Parameters"			#40
+"[41] Unmounted Partitions"			#41
+"[42] !DONE! thanks for using this scripty"	#42
 )
 function output() {
 clear
@@ -162,29 +163,22 @@ else
 	output 5
 	echo "[6] Created BIOS partition"
 fi
-#7--------------------------------------------------------------------------#
+#8--------------------------------------------------------------------------#
 if [[ $UEFI -gt 0 ]]
 then
 	BASE=512
 else
 	BASE=8
 fi
-#create swap partition
-#parted "$DRIVE" --script mkpart linux-swap $BASE"MiB" $((2086+BASE))"MiB"
-#parted "$DRIVE" --script name 2 swap
-#
-#output 7
-#8--------------------------------------------------------------------------#
+
 #create  root partition
 parted "$DRIVE" --script mkpart primary $BASE"MiB" 100%
 parted "$DRIVE" --script name 2 root
 #
 output 8
-#---------------------------------------------------------------------------#
+#9--------------------------------------------------------------------------#
 #format root partition
 mkfs.ext4 "$DRIVE"2
-#format swap partition
-#mkswap "$DRIVE"2
 if [[ $UEFI -gt 1 ]]
 then
 #format EFI partition
@@ -195,8 +189,6 @@ output 9
 #10-------------------------------------------------------------------------#
 #mount root partition
 mount "$DRIVE"2 /mnt
-#swapon swap partition
-#swapon "$DRIVE"2
 #
 output 10
 #11-------------------------------------------------------------------------#
@@ -300,7 +292,6 @@ output 23
 #24-------------------------------------------------------------------------#
 #add wheel group to sudoers
 echo "%wheel ALL=(ALL:ALL) ALL" >> /mnt/etc/sudoers
-#sed -i "s/# \(%wheel ALL=(ALL) ALL\)/\1/g" /mnt/etc/sudoers
 #
 output 24
 #25-------------------------------------------------------------------------#
@@ -786,8 +777,6 @@ XF86MonBrightnessDown
 # show desktop
 super + d
 	rofi -show window
-	
-
 EOF
 #
 output 33
@@ -994,16 +983,64 @@ printf "install snd_hda_intel /bin/false" >> /mnt/etc/modprobe.d/microphone_bloc
 chmod 600 /mnt/etc/modprobe.d/*.conf
 #
 output 39
-#39-------------------------------------------------------------------------#
+#40------------------------------------------------------------------------#
+cat >> /mnt/etc/sysctl.d/security.conf << EOF
+net.ipv4.ip_default_ttl=128
+net.ipv4.icmp_echo_ignore_all = 1
+kernel.kptr_restrict=1
+kernel.dmesg_restrict=1
+kernel.printk=3 3 3 3
+kernel.unprivileged_bpf_disabled=1
+net.core.bpf_jit_harden=2
+dev.tty.ldisc_autoload=0
+vm.unprivileged_userfaultfd=0
+kernel.kexec_load_disabled=1
+kernel.sysrq=4
+kernel.unprivileged_userns_clone=0
+kernel.perf_event_paranoid=3
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_rfc1337=1
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.conf.all.accept_redirects=0
+net.ipv4.conf.default.accept_redirects=0
+net.ipv4.conf.all.secure_redirects=0
+net.ipv4.conf.default.secure_redirects=0
+net.ipv6.conf.all.accept_redirects=0
+net.ipv6.conf.default.accept_redirects=0
+net.ipv4.conf.all.send_redirects=0
+net.ipv4.conf.default.send_redirects=0
+net.ipv4.conf.all.accept_source_route=0
+net.ipv4.conf.default.accept_source_route=0
+net.ipv6.conf.all.accept_source_route=0
+net.ipv6.conf.default.accept_source_route=0
+net.ipv6.conf.all.accept_ra=0
+net.ipv6.conf.default.accept_ra=0
+net.ipv4.tcp_sack=0
+net.ipv4.tcp_dsack=0
+net.ipv4.tcp_fack=0
+kernel.yama.ptrace_scope=2
+net.ipv4.tcp_timestamps=0
+kernel.core_pattern=|/bin/false
+fs.suid_dumpable=0
+vm.swappiness=1
+random.trust_cpu=off
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+ipv6.disable=1
+EOF
+#
+output 40
+
+#41-------------------------------------------------------------------------#
 #unmount partitions
-#swapoff "$DRIVE"2
 umount "$DRIVE"1
 umount "$DRIVE"2
 #
-output 40
+output 41
 ##-------------------------------------------------------------------------##
 #
-output 41
+output 42
 #reboot
 exit
 #--------------------------------------------------------------------------##
