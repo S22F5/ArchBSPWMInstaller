298f817ffe502dbb99df85e9b8371dcf15557b03
S22F5 <s22f5@mailfence.com>
2023-03-17 13:45:46 +0100

    added grub parameters

--- v2installer.sh
+++ v2installer.sh
@@ -313,10 +313,12 @@ then
 	arch-chroot /mnt mkdir /boot/EFI
 	arch-chroot /mnt mount "$DRIVE"1 /boot/EFI
 	arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --removable
+	sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=slab_nomerge init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on randomize_kstack_offset=on vsyscall=none debugfs=off oops=panic module.sig_enforce=1 lockdown=confidentiality quiet loglevel=0" /mnt/etc/default/grub
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 else
 	#install and setup grub2 for mbr/bios
 	arch-chroot /mnt grub-install --target=i386-pc "$DRIVE"
+	sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=slab_nomerge init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on randomize_kstack_offset=on vsyscall=none debugfs=off oops=panic module.sig_enforce=1 lockdown=confidentiality quiet loglevel=0" /mnt/etc/default/grub
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 fi
 #
